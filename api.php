fdd4d19625de52b383378d795feffa2a6cd201e4
DESKTOP-DEQKAE8\NATEE <teerapong@dnamaker.com>
2017-12-22 02:48:49 +0700

    update

--- api.php
+++ api.php
@@ -77,7 +77,7 @@ curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
 //Set the content type to application/json
 curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
 //curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
-//Execute the request
+
 if(!empty($input['entry'][0]['messaging'][0]['message'])){
     $result = curl_exec($ch);
 }
